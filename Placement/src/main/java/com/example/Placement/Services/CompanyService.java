fd9ce253667e665ac3f662810ef79882f0a02b74
Midhula <midhula295@gmail.com>
2026-04-20 13:37:55 +0530

    conflict resolved

--- Placement/src/main/java/com/example/Placement/Services/CompanyService.java
+++ Placement/src/main/java/com/example/Placement/Services/CompanyService.java
@@ -5,12 +5,20 @@ import com.example.Placement.Models.Company;
 import com.example.Placement.Repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 @Service
 public class CompanyService {
     @Autowired
     private CompanyRepository companyRepository;
+    private final String uploadDir = "uploads/";
 
     public String addCompany(List<Company> company) {
       companyRepository.saveAll(company);
